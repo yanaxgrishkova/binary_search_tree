0aa81c8038d2986b4fcf994a68a26b9a95f9c78a
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-04-10 07:42:57 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,7 +1,8 @@
 #include <binary_search_tree.hpp>
 #include <catch.hpp>
 
-SCENARIO("default constructor") {
+SCENARIO("default constructor") 
+{
 	Node<int> n;
 	REQUIRE(n.root() == nullptr);
 	REQUIRE(n.count() == 0);
@@ -24,3 +25,11 @@ SCENARIO("findElement")
 	REQUIRE(a == true);
 }
 
+SCENARIO("out")
+{
+	  Node<int> n1;
+	  n.out("file3.txt");
+	  Node<int> n2;
+	  n2.out("file3.txt");
+	  REQUIRE(n1.count() == n2.count());
+}
