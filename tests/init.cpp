ef23612c10ad71ad5e334c8a81712e7606cd3966
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-04-10 09:53:27 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,28 +8,3 @@ SCENARIO("default constructor")
 	REQUIRE(bst.count() == 0);
 }
 
-SCENARIO("insertElement")
-{
-	BinarySearchTree<int> bst;
-	bst.insert(7);
-	REQUIRE(bst.findElement() == 7);
-	REQUIRE(bst.count() == 1);
-}
-
-SCENARIO("findElement")
-{
-	BinarySearchTree<int> bst;
-	bool a;
-	bst.insert(7);
-	a = bst.isFound(7);
-	REQUIRE(a == true);
-}
-
-SCENARIO("out")
-{
-	  BinarySearchTree<int> bst1;
-	  bst1.out("file3.txt");
-	  BinarySearchTree<int> bst2;
-	  bst2.out("file3.txt");
-	  REQUIRE(bst1.count() == bst2.count());
-}
