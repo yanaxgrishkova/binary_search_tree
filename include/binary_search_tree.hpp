46ee15db892948fa5a476c90f5d57384bf8a45aa
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-04-10 13:19:51 +0300

    Update binary_search_tree.hpp

--- include/binary_search_tree.hpp
+++ include/binary_search_tree.hpp
@@ -149,7 +149,7 @@ void BinarySearchTree<T>::insertElement(Node<T>* &node, const T &value)
 template <typename T>
 void BinarySearchTree<T>::insert(const T& value)
 {
-	insertElement(root, value);
+	insertElement(root->value, value);
 }
 
 template <typename T>
